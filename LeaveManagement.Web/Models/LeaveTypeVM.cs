f4c62475432844bef542b2546604911403554da7
Kirill Topchy <kirilltopchy@gmail.com>
2022-11-05 13:51:33 +0200

    Create View setup.

--- LeaveManagement.Web/Models/LeaveTypeVM.cs
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -5,9 +5,14 @@ namespace LeaveManagement.Web.Models
     public class LeaveTypeVM
     {
         public int Id { get; set; }
+
         [Display(Name = "Leave Type Name")]
+        [Required]
         public string Name { get; set; }
+
         [Display(Name = "Default Numer Of Days")]
+        [Required]
+        [Range(1, 25, ErrorMessage = "Please Enter A Valid Number Of Days (1 - 25)")]
         public int DefaultDays { get; set; }
     }
 }
